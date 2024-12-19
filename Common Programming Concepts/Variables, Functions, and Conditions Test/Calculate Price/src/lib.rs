c45af990cdc421ed2a41fe261cde35fcadecefdd
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-19 13:42:46 -0500

    Finished Conditions and the Variables, Functions, and Conditions test

--- Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
+++ Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
@@ -1,3 +1,11 @@
-﻿pub fn calculate_price(/* TODO */) /*TODO*/ {
-    /*write your solution here*/
+﻿pub fn calculate_price(apples_to_order: i32) -> i32 {
+    let mut price_per_apple = 2;
+    if apples_to_order >= 40 {
+        price_per_apple = 1;
+    }
+    let total_price = apples_to_order * price_per_apple;
+    return total_price;
+    // let i = total_price;
+    // i
 }
+
