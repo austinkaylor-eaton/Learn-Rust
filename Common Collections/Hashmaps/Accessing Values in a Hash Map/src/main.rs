34d4b39ac59f272162904dc3cc0129aa16cadedf
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-23 16:56:34 -0500

    Completed the Common Collections - HashMaps section

--- Common Collections/Hashmaps/Accessing Values in a Hash Map/src/main.rs
+++ Common Collections/Hashmaps/Accessing Values in a Hash Map/src/main.rs
@@ -3,13 +3,15 @@
 fn main() {
     let mut scores = HashMap::new();
 
-    scores.insert(String::from("Blue"), 10);
+    let team_name = String::from("Blue");
+    
+    scores.insert(String::from(&team_name), 10);
     scores.insert(String::from("Yellow"), 50);
 
-    let team_name = String::from("Blue");
+    //let team_name = String::from("Blue");
     let score = scores.get(&team_name);
 
-    println!("{} team's score is {}", team_name, score.unwrap());
+    println!("{} team's score is {}", &team_name, score.unwrap());
 
     for (key, value) in &scores {
         println!("{}: {}", key, value);
