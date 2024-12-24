a3c85fb6c1f91945466f6443881ec62cb85f5d97
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-23 22:42:57 -0500

    Completed the section Error Handling - Error Handling

--- Error Handling/Error Handling/Matching Result 2/src/main.rs
+++ Error Handling/Error Handling/Matching Result 2/src/main.rs
@@ -4,7 +4,13 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = match total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(cost) => cost,
+        Err(err) => {
+            println!("Error: {}", err);
+            return;
+        }
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
