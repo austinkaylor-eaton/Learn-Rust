d529001ce89634af3990ba7ec641155f49e95186
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-27 11:33:07 -0500

    Completed the section Generic Types, Traits, and Lifetimes - Traits

--- Generic Types, Traits, and Lifetime/Traits/String Trait/src/lib.rs
+++ Generic Types, Traits, and Lifetime/Traits/String Trait/src/lib.rs
@@ -3,7 +3,10 @@
 }
 
 impl AppendBar for String {
-    /* Add your code here */
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
+    
 }
 
 
--- Generic Types, Traits, and Lifetime/Traits/Vec Trait/src/lib.rs
+++ Generic Types, Traits, and Lifetime/Traits/Vec Trait/src/lib.rs
@@ -2,4 +2,9 @@
     fn append_bar(self) -> Self;
 }
 
-/* Add your code here */
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        return self
+    }
+}
